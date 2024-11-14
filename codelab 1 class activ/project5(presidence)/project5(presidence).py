68c5e3ca832a4011fd4e187e90a5d6a12c881ae9
AmaanAnsari04 <amaanahansari@gmail.com>
2024-11-14 10:47:08 +0400

    Presidence

---
+++ codelab 1 class activ/project5(presidence)/project5(presidence).py
@@ -0,0 +1,27 @@
+# #when you have multiple operations in the same statement, it will follow the BODMAS rule
+# print ((4*3) + 6 //8*(6+9))
+
+# #to write formulas in multiple lines you need to add a backslash \
+# result = 4 + 7 + 8 + 9 +\
+#          7 + 9 + 5
+# print (result) 
+
+# #end delimiter to remove new line, by default print will switch to another statement so they will be printed on the same line
+# name = "Amaan Ansari"
+# print (name , end =" " )
+# print ("has been suspected ")
+
+# #if you use \n it will print on a new line and if you use \t it will print with space at the start
+# print ("\n new message")
+# print ("\t 4 spaces later...")
+# print ("\t \t even more spaces later")
+
+# #you can use f to format the output as you want
+# var = "drive"
+# print (f" he has the {var}" )
+
+# print (f'hello world')
+
+# fruit = "bananas"
+# print (f"I like to eat {fruit}")
+
