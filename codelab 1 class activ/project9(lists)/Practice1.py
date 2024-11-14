7a4ca5bdcef3b533d228c52287d7798ab4120d23
AmaanAnsari04 <amaanahansari@gmail.com>
2024-11-14 10:47:59 +0400

    Lists

---
+++ codelab 1 class activ/project9(lists)/Practice1.py
@@ -0,0 +1,20 @@
+#lists are used to hold muliple data items and can hold different types of data as well
+
+#making homogenous lists
+Names = ["Amaan" , "Sharjah" , "Creative computing" ]
+print (Names)
+
+Sales = [38.0 , 72.2 , 98.0]
+print (Sales)
+
+Score = [8 , 4 , 12 , 6 , 2]
+
+#making hetrogenous lists
+Info = ["car" , 37 , True , 7.9]
+print (Info)
+
+Loss = [38 , 89.3 , 21]
+print (Loss)
+
+Age = [37 , "thirteen" , 29 , "twenty four"]
+print (Age)
---
+++ codelab 1 class activ/project9(lists)/Practice2.py
@@ -0,0 +1,9 @@
+#using a repitition operator to duplicate lists
+
+Sales = [34, 45, 32 ,21, 55]
+Weekly_Sales = Sales * 7
+print (Weekly_Sales)
+
+Weekly_Customers = [12 , 5 , 14 , 8 , 9 ,20 , 21 ]
+Monthly_Customers = Weekly_Customers * 30
+print (Monthly_Customers) 
