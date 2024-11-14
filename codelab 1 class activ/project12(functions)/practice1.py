d81cbd38534bdbb5c84a5a745e56ba867b091cae
AmaanAnsari04 <amaanahansari@gmail.com>
2024-11-14 10:45:49 +0400

    functions

---
+++ codelab 1 class activ/project12(functions)/practice1.py
@@ -0,0 +1,11 @@
+#creating a local function
+def Morning_greeting():
+    greeting  = "Good morning"
+    print (greeting)
+print (Morning_greeting())
+
+#different functions can contain the same variables with no syntax error
+def Evening_greeting():
+    greeting = "Good Evening"
+    print(greeting)
+print(Evening_greeting())
---
+++ codelab 1 class activ/project12(functions)/practice3.py
@@ -0,0 +1,8 @@
+#you can pass the arguement to a variable
+
+def Greeting(name):
+    print(f"Hello {name}")
+
+Morning = "Good morning"
+
+Greeting(Morning)
