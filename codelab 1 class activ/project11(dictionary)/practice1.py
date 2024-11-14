4928be25336c9266e59ec1f0c560b777d979b8b1
AmaanAnsari04 <amaanahansari@gmail.com>
2024-11-14 10:45:26 +0400

    Dictionary

---
+++ codelab 1 class activ/project11(dictionary)/practice1.py
@@ -0,0 +1,13 @@
+Languages = {"Pakistan":"Urdu", 
+             "U.A.E":"Arabic",
+             "America":"English",
+             "Japan":"Japanese",
+             "Korea":"Korean",
+             "Russia":"Russian",
+             "Wales":"Welsh"}
+
+print (Languages)
+
+print (Languages.keys())
+
+print(Languages["Korea"])
---
+++ codelab 1 class activ/project11(dictionary)/practice2.py
@@ -0,0 +1,10 @@
+Languages = {"Pakistan":"Urdu", 
+             "U.A.E":"Arabic",
+             "America":"English",
+             "Japan":"Japanese",
+             "Korea":"Korean",
+             "Russia":"Russian",
+             "Wales":"Welsh"}
+
+del (Languages["Korea"])
+print (Languages.items())
